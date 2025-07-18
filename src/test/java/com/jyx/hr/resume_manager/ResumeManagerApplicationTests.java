2a13c599f78a4dbbf9e5107d3952ce4c2673a411
wajih <mohamedwajih.elouaer@gmail.com>
2025-07-18 21:41:24 +0100

    add project structure

---
+++ src/test/java/com/jyx/hr/resume_manager/ResumeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyx.hr.resume_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
